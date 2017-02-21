7a92406331ee18ee7662b2a141a17fbaa9ddaa94
Igor Pokorny <igor.pokorny.1@gmail.com>
2017-02-21 21:39:58 +0100

    Add tool to toggle between pack/unpack paragraphs after the third one

--- chekhov-jQuery.js
+++ chekhov-jQuery.js
@@ -9,4 +9,22 @@ function makeStoryHidden(storyElement) {
 	$(storyElement).children("p:eq(2)").append(newLink);
 	$(storyElement).children("p:gt(2)").hide();
 
+
+
+	newLink.attr("href","#");
+	newLink.attr( "class", "more-link" );
+
+	// newLink.on( "click", function(event) {
+	// 	event.preventDefault();
+	// }	
+	
+	var hiddenP = $(storyElement).children("p:gt(2)");
+	newLink.click(function(event){
+		event.preventDefault();
+    	$(hiddenP).toggle();
+	});
+    
+	// $( "a" ).toggle();
+
 }
+
